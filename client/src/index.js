ac95ce4bec4a4b7f34bc53cb334b9f9ea8516140
FedericoLeiva12 <federicoleivap@gmail.com>
2020-09-01 18:59:40 -0300

    MaterialUI Installed.

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Material UI requirements
+import 'fontsource-roboto';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
