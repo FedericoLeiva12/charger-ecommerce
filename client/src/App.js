f36579d49193f56b68a25cba2cf325a65df35782
FedericoLeiva12 <federicoleivap@gmail.com>
2020-09-02 19:14:59 -0300

    Rutas configuradas.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+// PAGES
+import HomePage from './pages/homePage';
+import BusquedaPage from './pages/busquedaPage';
+import CheckoutPage from './pages/checkoutPage';
+import ProductPage from './pages/productPage';
+import AdminPage from './pages/adminPage';
+import CategoriaPage from './pages/categoriaPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      {/* User Routes */}
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/busqueda/:query' component={BusquedaPage} />
+      <Route exact path='/categoria/:categoria' component={CategoriaPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
+      <Route exact path='/producto/:producto' component={ProductPage} />
+      {/* Admin Routes */}
+      <Route exact path='/admin' component={AdminPage} />
+    </Switch>
   );
 }
 
