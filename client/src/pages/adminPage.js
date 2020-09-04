affe26a1bf877ade9ba8261f46a99087967a2086
msebass1 <msebass1@gmail.com>
2020-09-04 15:24:26 -0500

    estilos añadidos

--- client/src/pages/adminPage.js
+++ client/src/pages/adminPage.js
@@ -1,50 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { getCategories, addCategory, deleteCategory, modifyCategory } from '../store/actions';
 import { connect } from 'react-redux';
+import FormCategorias from '../components/AdminForms/';
 
 function AdminPage({categories, getCategories, addCategory, deleteCategory, modifyCategory}) {
     const [name, setName] = useState('');
     const [id, setId] = useState(0);
 
     return (
-        <div>
-            <button onClick={getCategories}>Get Categories</button>
-
-            <form onSubmit={e => {
-                e.preventDefault();
-                addCategory(name);
-            }}>
-                <input onChange={e => setName(e.target.value)} value={name} placeholder='name'/>
-                <input type='submit'/>
-            </form>
-
-            <form onSubmit={e => {
-                e.preventDefault();
-                modifyCategory(id, name);
-            }}>
-                <input onChange={e => setName(e.target.value)} value={name} placeholder='name'/>
-                <input onChange={e => setId(e.target.value)} value={id} placeholder='id'/>
-                <input type='submit'/>
-            </form>
-
-            <form onSubmit={e => {
-                e.preventDefault();
-                deleteCategory(id);
-            }}>
-                <input onChange={e => setId(e.target.value)} value={id} placeholder='id'/>
-                <input type='submit'/>
-            </form>
-
-            <div>
-                {categories.map(
-                    function(cat, index){
-                        return (
-                            <div key={index}>{cat.id} {cat.name}</div>
-                        );
-                    }
-                )}
-            </div>
-        </div>
+    <div>
+      <FormCategorias 
+      viewCategories={getCategories}
+      categories={categories} 
+      name={name}
+      setName={setName}
+      id={id}
+      setId={setId}
+      addCategory={addCategory}
+      deleteCategory={deleteCategory}
+      modifyCategory={modifyCategory}
+      />
+    </div>
     )
 }
 
