f919080aa435e76217dc4cda321aa290cd179433
Juan Cruz <lesca5172@outlook.com>
2020-09-07 00:46:52 -0300

    pequeñitas modificaciones de UX

---
+++ client/src/components/arrayImgBG.js
@@ -0,0 +1,11 @@
+const backgroundImages = [
+  'https://images.unsplash.com/photo-1586396874197-b8fc676a5187?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1542053254535-def95e944232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1573120525707-4549889744f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1548437629-18ee60d53cd7?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1586396847415-2c76ae7e79fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1503342452485-86b7f54527ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  'https://images.unsplash.com/photo-1503341733017-1901578f9f1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+]
+
+export default backgroundImages;
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -2,12 +2,25 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Style from '../pages/homePage.module.css'
 import Container from '../components/NavBar/Container'
+import backgroundImages from '../components/arrayImgBG'
 
+const randomImg = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+
+const backgroundImageStyle = {
+    height: '100vh',
+    background: `url(${randomImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: '0% 50%',
+    color: 'white',
+    padding: '0px',
+    margin: '0px',
+}
 
 export default function HomePage() {
 
     return (
-        <div className={Style.imageBg}>
+        <div style={backgroundImageStyle}>
             <Container />
             <div className={Style.title}>
                 <h1>CHARGER</h1>
