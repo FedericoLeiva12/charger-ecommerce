5f25ccc46616bfe1751dc3e54dac4f742de2dc93
Juan Cruz <lesca5172@outlook.com>
2020-09-09 16:31:55 -0300

    get all users

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,8 +1,14 @@
 const server = require('express').Router();
-const { Product, Categories, Img } = require('../db.js');
+const { User, InfoUser, Roles } = require('../db.js');
 
 // All users
-
+server.get('/', (req, res, next) => {
+  User.findAll({include: InfoUser})
+    .then(users => {
+      res.status(200).render(users)
+    })
+    .catch(next)
+})
 // Create Users
 
 // Modify Users
