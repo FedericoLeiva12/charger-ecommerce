6bfcd797a0bb6d29565c7378bfb4d93edfdf3298
jotap29 <jotapmartinez29@gmail.com>
2020-09-09 16:50:53 -0300

    Create user route

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -10,5 +10,26 @@ server.get('/', (req, res, next) => {
     .catch(next)
 })
 // Create Users
-
+server.post('/', (req,res,next)=>{
+  const {email, password, name, lastName, address } = req.body
+  User.create({
+    email ,
+    password 
+  })
+  .then((user)=>{
+    user.addInfoUser({
+      name  ,
+      lastName ,
+      address
+    })
+  })
+  .then((createdUser)=>{
+    res.send({
+      text : 'User created succesfully!',
+      createdUser : createdUser.dataValues
+    })
+  }).catch(err => {
+    res.status(500).send({ text: err})
+  })
+})
 // Modify Users
