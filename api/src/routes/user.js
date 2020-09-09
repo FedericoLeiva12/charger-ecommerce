d67ef9f12ccc6431af28283072959c232fb81f09
jotap29 <jotapmartinez29@gmail.com>
2020-09-09 19:26:32 -0300

    Users routes

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -8,7 +8,7 @@ server.get('/', (req, res, next) => {
       res.status(200).render(users)
     })
     .catch(next)
-})
+});
 // Create Users
 server.post('/', (req,res,next)=>{
   const {email, password, name, lastName, address } = req.body
@@ -27,9 +27,59 @@ server.post('/', (req,res,next)=>{
     res.send({
       text : 'User created succesfully!',
       createdUser : createdUser.dataValues
-    })
+    }).status(201)
   }).catch(err => {
-    res.status(500).send({ text: err})
+    res.status(500).send({ text: err })
   })
-})
+});
 // Modify Users
+server.put('/:id', (req, res)=>{
+  const {email, password} = req.body
+  const {id} = req.params
+  if( !id  || !email || !password ) {
+		return res.status(400).send({ text: 'Invalid data' });
+  }
+  User.findOne({
+    where : {
+      id: parseInt(id)
+    }
+  }).then(user => {
+		user.email = email;
+		user.password = password;
+
+  }).then(user=>{
+    user.setInfoUser({
+      name  ,
+      lastName ,
+      address
+    })
+  }).then((user) => {
+    user.save()
+		res.send({ text: 'User updated.' });
+	}).catch(err => {
+		res.status(500).send({ text: 'Internal error' });
+		console.error(err);
+  }) 
+});
+
+//Remove Users
+
+server.delete('/:id', (req, res) => {
+	const { id } = req.params;
+
+	if(!id) {
+		return res.status(400).send({ text: 'Invalid id' });
+	}
+
+	User.destroy({
+		where: {
+			id: parseInt(id)
+		}
+	}).then(() => {
+		res.send({ text: 'User deleted'});
+	}).catch(() => {
+		res.status(500).send({ text: 'Internal error'});
+	})
+});
+
+module.exports = server;
