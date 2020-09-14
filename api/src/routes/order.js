b248fb11b8b513946eaa63020c48589e68861e1b
FedericoLeiva12 <federicoleivap@gmail.com>
2020-09-14 10:26:54 -0300

    Iniciadas ordenes

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,5 +1,18 @@
 const { Router } = require('express');
+const { User, Checkout } = require('../db');
 
 const router = Router();
+
+router.get('/:user', (req, res) => {
+    const { user } = req.params;
+    User.findOne({
+        where: {
+            id: user
+        },
+        include: Checkout
+    }).then(user => {
+        console.log(user);
+    }).catch(console.error)
+})
 
 module.exports = router;
