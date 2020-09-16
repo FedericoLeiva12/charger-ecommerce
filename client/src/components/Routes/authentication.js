fdcbf3ae4819f8f3f0c3d6dcee1abf2aa04575bf
jotap29 <jotapmartinez29@gmail.com>
2020-09-16 16:51:59 -0300

    Private routes

--- client/src/components/Routes/authentication.js
+++ client/src/components/Routes/authentication.js
@@ -1,6 +1,4 @@
-import { getUser } from "../../store/actions";
 
-
-export const isAuthenticated = () => getUser().logged ? true : false;
+export const isAuthenticated = (props) => props.logged ? true : false;
 
 //*tengo que hacer que esa function este true cuando esté logueado
--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const UserPage = ()=>{
     return(
         <>
-            User Pagez
+            User Page
         </>
     )
 }
