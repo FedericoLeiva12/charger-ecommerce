44c64c894790d5c2f78895a29f5910620c792076
FedericoLeiva12 <federicoleivap@gmail.com>
2020-09-17 14:05:09 -0300

    Responsive catalog

--- client/src/components/categorySearch/index.js
+++ client/src/components/categorySearch/index.js
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from "react";
-import Container from "./Container";
-import Selector from "../Selector";
+import Selector from "../components/Selector";
 import {
   createMuiTheme,
   ThemeProvider,
   makeStyles,
 } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-
-import NavBarCOntainer from "../NavBar/Container";
-import { Snackbar } from "@material-ui/core";
 import {
   getProducts,
   getSelectors,
   addToCart,
   removeFromCart,
-} from "../../store/actions";
+} from "../store/actions";
 import { connect } from "react-redux";
+import NavBarCOntainer from "../components/NavBar/Container";
+import { Snackbar } from "@material-ui/core";
+import CatalogContainer from "../components/catalogContainer";
 
 const darkTheme = createMuiTheme({
   palette: {
@@ -30,7 +29,14 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function Catalogo({ products, selectors, addToCart, cart, removeFromCart }) {
+function Catalogo({
+  products,
+  getProducts,
+  getSelectors,
+  selectors,
+  cart,
+  addToCart,
+}) {
   const classes = useStyles();
 
   const [alert, setAlert] = useState(false);
@@ -52,42 +58,42 @@ function Catalogo({ products, selectors, addToCart, cart, removeFromCart }) {
             alignItems="flex-start"
             paddingBottom="40px"
           >
-            {
-              /*selectors.map(cat=>{
-                  return(
-                    <Selector nom={cat.name} desc={cat.description} val={cat.id}/>
-                  )
-                })*/
+            {(() => {
+                if (selectors) {
+                  let results = [];
 
-               (() => {
-                  if(selectors) {
-                    let results = [];
+                  let i = 0;
 
-                    let i = 0;
-
-                    for(let [key, value] of Object.entries(selectors)) {
-                      results.push(<Selector key={i} nom={key} elements={value} />)
-                      i++;
-                    }
-
-                    return results
-                  } else {
-                    return ''
+                  for (let [key, value] of Object.entries(selectors)) {
+                    results.push(
+                      <Selector key={i} nom={key} elements={value} />
+                    );
+                    i++;
                   }
-                })()
-              }
-            </Grid>
-            <div>
-                <Container
-		setAlert={setAlert} 
-		products={products.map(prod => ({
-		  image: prod.imgs[0].url,
-      title: prod.name, price: prod.price,
-      name: prod.name, description: prod.description,
-		  id: prod.id, stock: prod.stock}))}
-		  addToCart={addToCart}
-		  removeFromCart={removeFromCart} />
-            </div>
+
+                  return results;
+                } else {
+                  return "";
+                }
+              })()
+            }
+          </Grid>
+          <div>
+            <CatalogContainer
+              setAlert={setAlert}
+              products={products.map((prod) => ({
+                image: prod.imgs[0].url,
+                title: prod.name,
+                name: prod.name,
+                description: prod.description,
+                price: prod.price,
+                id: prod.id,
+                stock: prod.stock,
+                categories: prod.categories,
+              }))}
+              addToCart={addToCart}
+            />
+          </div>
         </div>
       </ThemeProvider>
       <Snackbar
--- client/src/pages/categoriaPage.js
+++ client/src/pages/categoriaPage.js
@@ -1,18 +1,49 @@
-import React from "react";
-import Catalogo from "../components/categorySearch/";
+import React, { useEffect, useState } from "react";
+import Selector from "../components/Selector";
 import {
-  categories,
-  products,
-  getProductsByCategory,
-  getCategories,
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import {
+  getProducts,
   getSelectors,
-} from "./../store/actions";
-import { useParams } from "react-router";
+  addToCart,
+  removeFromCart,
+  getProductsByCategory,
+} from "../store/actions";
 import { connect } from "react-redux";
-import { useEffect } from "react";
+import NavBarCOntainer from "../components/NavBar/Container";
+import { Snackbar } from "@material-ui/core";
+import CatalogContainer from "../components/catalogContainer";
+import { useParams } from "react-router-dom";
 
-function CategoriaPage({ selectors, products, getProducts, getSelectors }) {
-  let { categoryId } = useParams();
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const useStyles = makeStyles(() => ({
+  cont: {
+    background: "#3D3D3D",
+  },
+}));
+
+function CategoryPage({
+  products,
+  getProducts,
+  getSelectors,
+  selectors,
+  cart,
+  addToCart,
+}) {
+  const { categoryId } = useParams();
+
+  const classes = useStyles();
+
+  const [alert, setAlert] = useState(false);
 
   useEffect(() => {
     getProducts(categoryId);
@@ -20,15 +51,69 @@ function CategoriaPage({ selectors, products, getProducts, getSelectors }) {
   }, []);
 
   return (
-    <div>
-      <Catalogo
-        categoryId={categoryId}
-        selectors={selectors}
-        products={products}
-        getProducts={getProducts}
-        getSelectors={getSelectors}
+    <>
+      <NavBarCOntainer cart={cart} />
+      <div style={{ paddingTop: 64, backgroundColor: "#3D3D3D" }}></div>
+      <ThemeProvider theme={darkTheme}>
+        <div className={classes.cont}>
+          <Grid
+            container
+            direction="row"
+            alignItems="flex-start"
+            paddingBottom="40px"
+          >
+            {
+              /*selectors.map(cat=>{
+                  return(
+                    <Selector nom={cat.name} desc={cat.description} val={cat.id}/>
+                  )
+                })*/
+
+              (() => {
+                if (selectors) {
+                  let results = [];
+
+                  let i = 0;
+
+                  for (let [key, value] of Object.entries(selectors)) {
+                    results.push(
+                      <Selector key={i} nom={key} elements={value} />
+                    );
+                    i++;
+                  }
+
+                  return results;
+                } else {
+                  return "";
+                }
+              })()
+            }
+          </Grid>
+          <div>
+            <CatalogContainer
+              setAlert={setAlert}
+              products={products.map((prod) => ({
+                image: prod.imgs[0].url,
+                title: prod.name,
+                name: prod.name,
+                description: prod.description,
+                price: prod.price,
+                id: prod.id,
+                stock: prod.stock,
+                categories: prod.categories,
+              }))}
+              addToCart={addToCart}
+            />
+          </div>
+        </div>
+      </ThemeProvider>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={alert}
+        onClose={(e) => setAlert(false)}
+        message="Out of stock"
       />
-    </div>
+    </>
   );
 }
 
@@ -47,4 +132,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CategoriaPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryPage);
