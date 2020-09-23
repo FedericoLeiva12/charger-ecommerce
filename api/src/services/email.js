5995a0426831706f0344ca078d5a6ff55959a4ed
FedericoLeiva12 <federicoleivap@gmail.com>
2020-09-23 17:27:05 -0300

    Working email service

--- api/src/services/email.js
+++ api/src/services/email.js
@@ -10,21 +10,26 @@ module.exports = function sendEmail(data, vars) {
         let content = fs.readFileSync(path.join(__dirname, data.content), 'utf-8');
 
         for(let [key, value] of Object.entries(vars)) {
-            while(content.indexOf(`{${key}}`) >= 0) {
-                content.replace(`{${key}}`, value);
+            while(content.indexOf(`{${key}}`) !== -1) {
+                content = content.replace(`{${key}}`, value);
             }
         }
+
+        console.log(content)
 
         mg.messages().send({
             from: `Changer <${data.from}@${DOMAIN}>`,
             to: data.to,
             subject: data.subject,
+            text: content,
             html: content
         }, (err, response) => {
-            if(err)
+            if(err) {
                 rej(err);
-            else
+                }
+            else {
                 res(response);
+            }
         });
     })
 }
