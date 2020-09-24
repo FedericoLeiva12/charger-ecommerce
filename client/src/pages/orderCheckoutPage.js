e7af8a876e250cd9c21f45542d114849817b9c5f
JuanCruzLescano <lesca5172@outlook.com>
2020-09-24 08:07:01 -0300

    order confirmation page done

--- client/src/pages/orderCheckoutPage.js
+++ client/src/pages/orderCheckoutPage.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Box, Card, CardContent, makeStyles } from '@material-ui/core';
-import { LocalGroceryStore } from '@material-ui/icons';
+import React from 'react'
+import {
+    Box,
+    Card,
+    CardContent,
+    makeStyles,
+    Paper,
+    Typography,
+} from '@material-ui/core'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import Style from './orderCheckoutPage.module.css'
 
 const useStyle = makeStyles({
     root: {
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000'
+        // width: '100vw',
+        // height: '100vh',
+        // display: 'flex',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // backgroundColor: '#e0e0e0',
     },
     card: {
         maxWidth: '480px',
-        boxShadow: '5px 5px #ddd'
+        boxShadow: '8px 8px 8px -8px rgba(33,33,33,0.5);',
+        borderRadius: '8px',
+        backgroundColor: '#fafafa'
     },
     content: {
         display: 'flex',
@@ -24,21 +34,33 @@ const useStyle = makeStyles({
     icon: {
         marginRight: 'auto',
         marginLeft: 'auto',
-        fontSize: '2.5em',
-        color: 'green'
+        fontSize: '2em',
+        color: '#f50057',
+    },
+    infoText: {
+        marginRight: '32px',
+        marginLeft: '32px',
+        color: '#212121'
+    },
+    titleText: {
+        color: '#212121'
     }
-});
+})
 
 export default () => {
-    const classes = useStyle();
+    const classes = useStyle()
     return (
-        <Box className={classes.root}>
-            <Card className={classes.card}>
+        <Box className={Style.root}>
+            <Paper className={classes.card}>
                 <CardContent className={classes.content}>
-                    <LocalGroceryStore className={classes.icon} />
-                    We have sent you an email. Open it to confirm the purchase.
+                    <FavoriteBorderIcon className={classes.icon} />
+                    <Typography variant="h2" className={classes.titleText}>THANK YOU!</Typography>
+                    <Typography variant="h6" className={classes.infoText}>
+                        We have sent you an email. Open it to confirm the
+                        purchase!
+                    </Typography>
                 </CardContent>
-            </Card>
+            </Paper>
         </Box>
     )
 }
