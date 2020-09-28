107b2b8d1c15704f878c0f45efc6124735ff068b
JuanCruzLescano <lesca5172@outlook.com>
2020-09-28 10:53:33 -0300

    mas estilo que Riqui For

--- client/src/components/Payment/ShippingAndPaymentForm.js
+++ client/src/components/Payment/ShippingAndPaymentForm.js
@@ -1,22 +1,24 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Button } from "@material-ui/core";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import {makeStyles} from '@material-ui/core/styles'
+import {Grid, Button} from '@material-ui/core'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
 import CardForm from './DialogStripeCC'
-import { Elements } from "@stripe/react-stripe-js";
+import {Elements} from '@stripe/react-stripe-js'
 import {loadStripe} from '@stripe/stripe-js'
-import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
-const stripePromise= loadStripe('pk_test_51HVqxeCJbTm2zZUewzVqUxrwjYz9MnzEYwZUJ2QCBcbjfzWyNLZT8vut0cXlIt28Gn6HGssqUZercMaRa0hplIqF00c5g6uOth')
-const useStyles = makeStyles((theme) => ({
+import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
+const stripePromise = loadStripe(
+  'pk_test_51HVqxeCJbTm2zZUewzVqUxrwjYz9MnzEYwZUJ2QCBcbjfzWyNLZT8vut0cXlIt28Gn6HGssqUZercMaRa0hplIqF00c5g6uOth'
+)
+const useStyles = makeStyles(theme => ({
   root: {
-    "& .MuiTextField-root": {
+    '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: "25ch",
+      width: '25ch',
     },
   },
   formControl: {
@@ -24,18 +26,29 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 120,
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(1),
   },
-}));
+  textField: {
+    margin: theme.spacing(1)
+  }
+}))
 
-export default function ShippingAndPaymentForm({shippingAdress, purchaseMethod, setPurchaseData, setPaymentMethodDos, setShippingAdress, setPurchaseMethod, doPayment }) {
-  const classes = useStyles();
-  
-  const handleChangeSelect = (event) => {
-    setPurchaseMethod(event.target.value);
-  };
-  const handleChangeShippingAdress = (event) => {
-    setShippingAdress(event.target.value);
+export default function ShippingAndPaymentForm({
+  shippingAdress,
+  purchaseMethod,
+  setPurchaseData,
+  setPaymentMethodDos,
+  setShippingAdress,
+  setPurchaseMethod,
+  doPayment,
+}) {
+  const classes = useStyles()
+
+  const handleChangeSelect = event => {
+    setPurchaseMethod(event.target.value)
+  }
+  const handleChangeShippingAdress = event => {
+    setShippingAdress(event.target.value)
   }
 
   return (
@@ -44,7 +57,7 @@ export default function ShippingAndPaymentForm({shippingAdress, purchaseMethod, 
         className={classes.root}
         noValidate
         autoComplete="off"
-/*         onSubmit={(e) => {
+        /*         onSubmit={(e) => {
           e.preventDefault();
           setPurchaseData(paymentMethod, shippingAdress);
         }} */
@@ -58,40 +71,40 @@ export default function ShippingAndPaymentForm({shippingAdress, purchaseMethod, 
           >
             <Grid item={12}>
               <TextField
-              required
-              label="Adress"
-              onChange={handleChangeShippingAdress}
-              value={shippingAdress}
-              placeholder="Exampleadress 1489"
-              helperText="Complete the shipping adress"
-              fullWidth
-              margin="normal"
-              color="secondary"
+                required
+                label="Address"
+                className={classes.textField}
+                onChange={handleChangeShippingAdress}
+                value={shippingAdress}
+                placeholder="Example address 1489"
+                helperText="Complete the shipping address"
+                fullWidth
+                margin="normal"
+                color="secondary"
               />
               <FormControl required fullWidth className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">Payment</InputLabel>
+                <InputLabel id="demo-simple-select-label" color='secondary'>Payment</InputLabel>
                 <Select
                   label="Payment Method"
                   id="demo-simple-select"
+                  color='secondary'
                   value={purchaseMethod}
                   onChange={handleChangeSelect}
                 >
-                  <MenuItem value={"Credit Card"}>Credit Card</MenuItem>
+                  <MenuItem value={'Credit Card'}>Credit Card</MenuItem>
                   <MenuItem value={null}>------------</MenuItem>
                 </Select>
                 <FormHelperText> Choose a payment method</FormHelperText>
               </FormControl>
-                {purchaseMethod=== 'Credit Card'?
+              {purchaseMethod === 'Credit Card' ? (
                 <Elements stripe={stripePromise}>
-                  <CardForm  doPayment={doPayment} />
+                  <CardForm doPayment={doPayment} />
                 </Elements>
-                : null}
-              </Grid>
-
+              ) : null}
+            </Grid>
           </Grid>
         </div>
       </form>
     </>
-  );
-};
-
+  )
+}
